392783072c39cbc2adb660cc597fce6143c71576
A-DBN <adabin@hotmail.fr>
2025-02-01 01:51:42 +0100

    fix: Change req.body last_update variable name to last_updated

--- src/controllers/mangasControllers.ts
+++ src/controllers/mangasControllers.ts
@@ -96,10 +96,10 @@ export const updateMangaController = async (
 };
 
 export const updateMangaChapterController = async (req: Request, res: Response) => {
-    const { name, chapter, last_update } = req.body;
+    const { name, chapter, last_updated } = req.body;
 
     try {
-        await updateMangaChapter(name, chapter as string, last_update);
+        await updateMangaChapter(name, chapter as string, last_updated);
         return res.status(200).send("Manga chapter updated");
     } catch (error) {
         return res.status(500).send((error as Error).message);
